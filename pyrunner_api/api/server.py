18d4e90cdfc85f1b8d661aec207cfec736823ed4
Merlin Webster <mjftwebster@gmail.com>
2020-08-19 22:40:53 +0100

    Add code to run python from API request

--- pyrunner_api/api/server.py
+++ pyrunner_api/api/server.py
@@ -1,8 +1,16 @@
-from flask import Flask, Response
+from flask import Flask, jsonify, request
+
+from ..runner.pyrunner import PyRunner
 
 app = Flask(__name__)
 
 
 @app.route('/run/python', methods=['POST'])
 def run_python():
-    return Response('', 200)
+    runner = PyRunner()
+    code = request.data
+    result = runner.run(code)
+
+    print(code, result)
+
+    return jsonify({'stdout': result.stdout, 'stderr': ''})
--- tests/api/test_server.py
+++ tests/api/test_server.py
@@ -1,5 +1,7 @@
 from flask.app import Flask
 import pytest
+import json
+
 from pyrunner_api.api import server
 
 
@@ -16,9 +18,32 @@ def test_server_should_respond_to_root_request(client):
     assert response.data
 
 
-def test_server_should_respond_status_200_to_post_run_python_endpoint(client):
+def test_server_run_python_should_respond_status_200_to_post(client):
     code = '#'
-    response = client.post('/run/python', data={
-        'code': code
-    })
+    response = client.post('/run/python', data='')
     assert 200 == response.status_code
+
+
+def test_server_run_python_should_respond_with_json_data(client):
+    response = client.post('/run/python', data='')
+
+    # Check no json.decoder.JSONDecodeError is raised
+    json.loads(response.data)
+
+
+def test_server_run_python_should_have_stdout_in_data(client):
+    response = client.post('/run/python', data='')
+
+    assert 'stdout' in json.loads(response.data)
+
+
+def test_server_run_python_should_have_stderr_in_data(client):
+    response = client.post('/run/python', data='')
+
+    assert 'stderr' in json.loads(response.data)
+
+
+def test_server_run_python_should_return_python_stdout(client):
+    response = client.post('/run/python', data='print("Hello World!")')
+
+    assert 'Hello World!\n' == json.loads(response.data).get('stdout')
