3be6dc652cd9a780b8ee4e3477c688079bed1d08
Merlin Webster <mjftwebster@gmail.com>
2020-08-19 19:51:59 +0100

    Remove pass statement from interface code

--- pyrunner_api/runner/interfaces/irunner.py
+++ pyrunner_api/runner/interfaces/irunner.py
@@ -2,8 +2,7 @@ import abc
 
 
 class IRunner(metaclass=abc.ABCMeta):
-    ''' Interface for code runners '''
+    '''Interface for code runners'''
     @abc.abstractmethod
     def run(self, code: str) -> str:
-        ''' Run an independent piece of code and return the stdout output'''
-        pass
+        '''Run an independent piece of code and return the stdout output'''
